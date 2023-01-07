68bca69e099bbe66eb43a49579bac51e3d83b7a4
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-01-07 18:49:52 +0000

    feat: add a collapse menu option on smaller screen

---
+++ src/components/commons/Menu/AddNote.tsx
@@ -0,0 +1,33 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+import styles from './menu.module.scss';
+
+interface IProps {
+  addNote: (title: string, content?: string) => void;
+}
+
+const AddNote = ({ addNote }: IProps) => {
+  return (
+    <form
+      className={styles['cmp-menu__add-note']}
+      onSubmit={(ev) => {
+        ev.preventDefault();
+        const form = ev.target as HTMLFormElement;
+        const imputEl = form.getElementsByTagName('input')[0] as HTMLInputElement;
+        addNote(imputEl.value);
+        form.reset();
+      }}>
+      <input
+        placeholder="add a note title"
+        aria-label="note Title"
+        className={styles['cmp-menu__add-note-input']}
+        name="note"
+      />
+      <button className={styles['cmp-menu__add-note-button']} type="submit">
+        <AiOutlinePlus size="1.2em" className={styles['cmp-menu__add-note-icon']} />
+        <span className={styles['cmp-menu__add-note-text']}>Add note</span>
+      </button>
+    </form>
+  );
+};
+
+export default AddNote;
--- src/components/commons/Menu/Menu.tsx
+++ src/components/commons/Menu/Menu.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { INote, IUseNotes } from '../../../hooks/useNotes';
 import styles from './menu.module.scss';
-import { AiOutlinePlus, AiOutlineFileText, AiFillDelete } from 'react-icons/ai';
+import { AiOutlinePlus, AiOutlineFileText, AiFillDelete, AiOutlineMenu, AiOutlineDoubleLeft } from 'react-icons/ai';
 import Link from 'next/link';
 import Button from '../../foundation/Button/Button';
+import AddNote from './AddNote';
 
 interface IProps {
   noteActions: IUseNotes;
@@ -11,54 +12,69 @@ interface IProps {
 
 const Menu = ({ noteActions }: IProps) => {
   const { notes, addNote, deleteNote } = noteActions;
+  const [menuVisible, setMenuVisible] = useState(true);
   const linkPath = (noteId: string) => `/?noteId=${noteId}`;
 
   const onRemove = (note: INote) => {
     deleteNote(note);
   };
 
+  const toggleMenu = () => {
+    console.log(menuVisible);
+    setMenuVisible(!menuVisible);
+  };
+
+  useEffect(() => {
+    const isSmallerScreen = window.matchMedia(`(max-width: ${768}px)`);
+    if (!isSmallerScreen.matches) {
+      setMenuVisible(false);
+    }
+  }, []);
+
+  const renderNoteList = (
+    <ul className={styles['cmp-menu__list']}>
+      {notes &&
+        notes.map((note) => (
+          <div key={note.id} className={styles['cmp-menu__list-items']}>
+            <Link href={linkPath(note.id)} className={styles['cmp-menu__list-items-link']}>
+              <div>
+                <AiOutlineFileText className={styles['cmp-menu__list-items-icon']} />
+              </div>
+              <span className={styles['cmp-menu__list-items-text']}>{note.title}</span>
+            </Link>
+
+            <Button iconOnly className={styles['cmp-menu__list-items-remove']} onClick={() => onRemove(note)}>
+              <AiFillDelete className={styles['cmp-menu__list-items-remove-icon']} />
+              <span className={styles['cmp-menu__list-items-remove-text']}>Delete note</span>
+            </Button>
+          </div>
+        ))}
+    </ul>
+  );
+
   return (
-    <nav className={styles['cmp-menu']}>
-      <form
-        className={styles['cmp-menu__add-note']}
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          const form = ev.target as HTMLFormElement;
-          const imputEl = form.getElementsByTagName('input')[0] as HTMLInputElement;
-          addNote(imputEl.value);
-          form.reset();
-        }}>
-        <input
-          placeholder="add a note title"
-          aria-label="note Title"
-          className={styles['cmp-menu__add-note-input']}
-          name="note"
-        />
-        <button className={styles['cmp-menu__add-note-button']} type="submit">
-          <AiOutlinePlus size="1.2em" className={styles['cmp-menu__add-note-icon']} />
-          <span className={styles['cmp-menu__add-note-text']}>Add note</span>
-        </button>
-      </form>
-
-      <ul className={styles['cmp-menu__list']}>
-        {notes &&
-          notes.map((note) => (
-            <div key={note.id} className={styles['cmp-menu__list-items']}>
-              <Link href={linkPath(note.id)} className={styles['cmp-menu__list-items-link']}>
-                <div>
-                  <AiOutlineFileText className={styles['cmp-menu__list-items-icon']} />
-                </div>
-                <span className={styles['cmp-menu__list-items-text']}>{note.title}</span>
-              </Link>
-
-              <Button iconOnly className={styles['cmp-menu__list-items-remove']} onClick={() => onRemove(note)}>
-                <AiFillDelete className={styles['cmp-menu__list-items-remove-icon']} />
-                <span className={styles['cmp-menu__list-items-remove-text']}>Delete note</span>
-              </Button>
-            </div>
-          ))}
-      </ul>
-    </nav>
+    <>
+      <Button
+        iconOnly
+        className={`${styles['cmp-menu__hamburger']} ${!menuVisible ? styles['cmp-menu__hamburger--opened'] : ''}`}
+        onClick={toggleMenu}>
+        {menuVisible ? (
+          <>
+            <AiOutlineMenu size="1.2em" className={styles['cmp-menu__hamburger-icon--closed']} />
+            <span className={styles['cmp-menu__hamburger-text']}>Close Menu</span>
+          </>
+        ) : (
+          <>
+            <AiOutlineDoubleLeft size="1.2em" className={styles['cmp-menu__hamburger-icon--opened']} />
+            <span className={styles['cmp-menu__hamburger-text']}>Open Menu</span>
+          </>
+        )}
+      </Button>
+      <nav className={`${styles['cmp-menu']} ${menuVisible ? styles['cmp-menu--collapsed'] : ''}`}>
+        <AddNote addNote={addNote} />
+        {renderNoteList}
+      </nav>
+    </>
   );
 };
 
