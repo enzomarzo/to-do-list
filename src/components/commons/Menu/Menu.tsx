6e49cfe1e6f0d4706c2eb8ee763f05f2ce397b14
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-01-08 11:47:11 +0000

    refactor: make the menu component more readable

--- src/components/commons/Menu/Menu.tsx
+++ src/components/commons/Menu/Menu.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { INote, IUseNotes } from '../../../hooks/useNotes';
 import styles from './menu.module.scss';
-import { AiOutlinePlus, AiOutlineFileText, AiFillDelete, AiOutlineMenu, AiOutlineDoubleLeft } from 'react-icons/ai';
+import { AiOutlineFileText, AiFillDelete, AiOutlineMenu, AiOutlineDoubleLeft } from 'react-icons/ai';
 import Link from 'next/link';
 import Button from '../../foundation/Button/Button';
 import AddNote from './AddNote';
@@ -15,14 +15,9 @@ const Menu = ({ noteActions }: IProps) => {
   const [menuVisible, setMenuVisible] = useState(true);
   const linkPath = (noteId: string) => `/?noteId=${noteId}`;
 
-  const onRemove = (note: INote) => {
-    deleteNote(note);
-  };
+  const onRemove = (note: INote) => deleteNote(note);
 
-  const toggleMenu = () => {
-    console.log(menuVisible);
-    setMenuVisible(!menuVisible);
-  };
+  const toggleMenu = () => setMenuVisible(!menuVisible);
 
   useEffect(() => {
     const isSmallerScreen = window.matchMedia(`(max-width: ${768}px)`);
@@ -31,25 +26,35 @@ const Menu = ({ noteActions }: IProps) => {
     }
   }, []);
 
-  const renderNoteList = (
+  const noteItems = (
     <ul className={styles['cmp-menu__list']}>
-      {notes &&
-        notes.map((note) => (
-          <div key={note.id} className={styles['cmp-menu__list-items']}>
-            <Link href={linkPath(note.id)} className={styles['cmp-menu__list-items-link']}>
-              <div>
-                <AiOutlineFileText className={styles['cmp-menu__list-items-icon']} />
-              </div>
-              <span className={styles['cmp-menu__list-items-text']}>{note.title}</span>
-            </Link>
-
-            <Button iconOnly className={styles['cmp-menu__list-items-remove']} onClick={() => onRemove(note)}>
-              <AiFillDelete className={styles['cmp-menu__list-items-remove-icon']} />
-              <span className={styles['cmp-menu__list-items-remove-text']}>Delete note</span>
-            </Button>
-          </div>
-        ))}
+      {notes?.map((note) => (
+        <div key={note.id} className={styles['cmp-menu__list-items']}>
+          <Link href={linkPath(note.id)} className={styles['cmp-menu__list-items-link']}>
+            <div>
+              <AiOutlineFileText className={styles['cmp-menu__list-items-icon']} />
+            </div>
+            <span className={styles['cmp-menu__list-items-text']}>{note.title}</span>
+          </Link>
+          <Button iconOnly className={styles['cmp-menu__list-items-remove']} onClick={() => onRemove(note)}>
+            <AiFillDelete className={styles['cmp-menu__list-items-remove-icon']} />
+            <span className={styles['cmp-menu__list-items-remove-text']}>Delete note</span>
+          </Button>
+        </div>
+      ))}
     </ul>
+  );
+
+  const menuToggle = menuVisible ? (
+    <>
+      <AiOutlineMenu size="1.2em" className={styles['cmp-menu__hamburger-icon--closed']} />
+      <span className={styles['cmp-menu__hamburger-text']}>Open Menu</span>
+    </>
+  ) : (
+    <>
+      <AiOutlineDoubleLeft size="1.2em" className={styles['cmp-menu__hamburger-icon--opened']} />
+      <span className={styles['cmp-menu__hamburger-text']}>Close Menu</span>
+    </>
   );
 
   return (
@@ -58,21 +63,11 @@ const Menu = ({ noteActions }: IProps) => {
         iconOnly
         className={`${styles['cmp-menu__hamburger']} ${!menuVisible ? styles['cmp-menu__hamburger--opened'] : ''}`}
         onClick={toggleMenu}>
-        {menuVisible ? (
-          <>
-            <AiOutlineMenu size="1.2em" className={styles['cmp-menu__hamburger-icon--closed']} />
-            <span className={styles['cmp-menu__hamburger-text']}>Close Menu</span>
-          </>
-        ) : (
-          <>
-            <AiOutlineDoubleLeft size="1.2em" className={styles['cmp-menu__hamburger-icon--opened']} />
-            <span className={styles['cmp-menu__hamburger-text']}>Open Menu</span>
-          </>
-        )}
+        {menuToggle}
       </Button>
       <nav className={`${styles['cmp-menu']} ${menuVisible ? styles['cmp-menu--collapsed'] : ''}`}>
         <AddNote addNote={addNote} />
-        {renderNoteList}
+        {noteItems}
       </nav>
     </>
   );
